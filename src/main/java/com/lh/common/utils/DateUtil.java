cbc6f461a858401f33623c009b0db75a773f29db
LIBAILUHUI <839035552@qq.com>
2020-03-27 15:19:42 +0800

    DateUtil增加

--- src/main/java/com/lh/common/utils/DateUtil.java
+++ src/main/java/com/lh/common/utils/DateUtil.java
@@ -2,8 +2,15 @@ package com.lh.common.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
+/** 
+ * @ClassName: DateUtil 
+ * @Description: TODO
+ * @author: Administrator
+ * @date: 2020年3月27日 上午10:00:20  
+ */
 public class DateUtil {
 
 	public static String generateDate() {
@@ -40,5 +47,90 @@ public class DateUtil {
 		//随机产生一个在t1和t2之间的毫秒数
 		long t = (long) ((Math.random() * (endTime-startTime+1)) +startTime);
 		return new Date(t);
+	}
+	
+	/**
+	 * 
+	 * @Title: getInitMonth 
+	 * @Description: 返回传入日期的月初 如传入2020/3/27 返回2020/3/01 00:00:00
+	 * @return
+	 * @return: Date
+	 */
+	public static Date getInitMonth(Date date) {
+		//获取一个日历类，相当于空白的东西，你去给它赋值
+		Calendar c = Calendar.getInstance();
+		//用传入的日期初始化日历类
+		c.setTime(date);
+		//设置日历的开始日期为第一天
+		c.set(Calendar.DAY_OF_MONTH, 1);
+		//设置天的第0个小时
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		//设置天的0分钟
+		c.set(Calendar.MINUTE, 0);
+		//设置天的0秒
+		c.set(Calendar.SECOND, 0);
+		return c.getTime();
+		
+	}
+	
+	/**
+	 * 
+	 * @Title: getEndMonth 
+	 * @Description: 返回一个月的月末，返回2020/3/31 23:59:59
+	 * @param date
+	 * @return
+	 * @return: Date
+	 */
+	public static Date getEndMonth(Date date) {
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+		//让当前月份加1变成4/27  变成月初2020/4/1 00:00:00 再减去1秒
+		c.add(Calendar.MONTH, 1);
+		//变成月初
+		Date initMonth = getInitMonth(c.getTime());
+		//先再次初始化，给值
+		c.setTime(initMonth);
+		//减1秒
+		c.add(Calendar.SECOND, -1);
+		return c.getTime();
+	}
+	
+	/**
+	 * 
+	 * @Title: getAgeByBirthday 
+	 * @Description: 根据出生日期算年龄
+	 * @param birthday
+	 * @return
+	 * @return: int
+	 */
+	public static int getAgeByBirthday(Date birthday) {
+		
+		
+		//获取日历类
+		Calendar c = Calendar.getInstance();
+		//获取系统的年
+		int sysYear = c.get(Calendar.YEAR);
+		int sysMonth = c.get(Calendar.MONTH);
+		int sysDay = c.get(Calendar.DAY_OF_MONTH);
+		
+		//初始化生日的值，可以获取生日的年月日
+		c.setTime(birthday);
+		//获取年
+		int birthYear = c.get(Calendar.YEAR);
+		int birthMonth = c.get(Calendar.MONTH);
+		int birthDay = c.get(Calendar.DAY_OF_MONTH);
+		int age = sysYear-birthYear;
+		
+		//如果系统月份小于出生月份
+		if(sysMonth < birthMonth) {
+			//年龄减少一岁
+			age--;
+		}
+		//如果月份相等，日期还没到，年龄也减1
+		if(sysMonth == birthMonth && sysDay <birthDay) {
+			age--;
+		}
+		
+		return age;
 	}
 }
--- src/test/java/com/lh/common/utils/DateUtilTest.java
+++ src/test/java/com/lh/common/utils/DateUtilTest.java
@@ -21,6 +21,7 @@ public class DateUtilTest {
 	@Test
 	public void testRandomDate() {
 		Calendar calendar = Calendar.getInstance();
+		//起始日期,2010年1月1日
 		calendar.set(2010, 0, 1);
 		Date date = DateUtil.randomDate(calendar.getTime(), new Date());
 	
@@ -28,5 +29,32 @@ public class DateUtilTest {
 		
 		System.out.println(sdf.format(date));
 	
+	}
+	
+	@Test
+	public void testGetInitMonth() {
+		Date date = DateUtil.getInitMonth(new Date(100000000l));
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(date));
+		
+	}
+	
+	//月末
+	@Test
+	public void testGetEndMonth() {
+		Calendar date1 = Calendar.getInstance();
+		date1.set(2020, 1, 21);
+		
+		Date date = DateUtil.getEndMonth(date1.getTime());
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println(sdf.format(date));
+		
+	}
+	
+	@Test
+	public void testGetAgeByBirthday() {
+		Calendar date1 = Calendar.getInstance();
+		date1.set(2010, 2, 27);
+		System.out.println(DateUtil.getAgeByBirthday(date1.getTime()));
 	}
 }
